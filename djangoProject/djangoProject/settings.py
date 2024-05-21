7673b8a85e3775bf1ef76257ae7b2215ac0d94c5
RezaCharrakh <116076795+RezaCharrakh@users.noreply.github.com>
2024-05-21 04:17:19 +0330

    creating users app and creating User model and sign up and sign in forms.

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projectApp',
+    'users'
 ]
 
 MIDDLEWARE = [
--- djangoProject/projectApp/models.py
+++ djangoProject/projectApp/models.py
@@ -65,4 +65,7 @@ class Cart(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"Cart for {self.user}\n"
+        return f"Cart for {self.user}"
+
+
+
